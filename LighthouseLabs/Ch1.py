4fd404bcc1776a95a1daf634b26404fdcb093440
reerpina <ramesh.eerpina@gmail.com>
2019-04-12 15:58:21 -0400

    Tax calculator

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,10 +1,11 @@
-a=13
-b=5
-#sum
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a//b)
-print(a**b)
-print(a%b)
+subtotal = float(input("Please input yout subtotal: "))
+tip = float(input("Please enter your tip percent: "))
+people= float(input("How many people attended the party: "))
+taxrate  = 0.25
+tipamount = subtotal * (tip/100)
+# use round function to round the decimal places to 2
+tax  = round(subtotal*taxrate, 2)
+total = round(subtotal + tax, 2) + tipamount
+ppp = total/people
+print("Each person should pay: " + str(ppp))
+print("Your total amount is: " + str(total))
