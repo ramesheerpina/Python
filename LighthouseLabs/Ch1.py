a6b16791d6db959ae1bf9286ca978f54014c05b3
reerpina <ramesh.eerpina@gmail.com>
2019-04-12 14:56:31 -0400

    Exercise String Replace

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,7 +1,14 @@
-material = 'snow'
-shape = 'ball'
-projectile = material + shape
-print(projectile + " fight")
-
-print('2+2')
-print('"2"+"2"')
+journey = """Just a small tone girl
+Leaving in a lonely whirl
+She took the midnight tray going anywhere
+Just a seedy boy
+Bored and raised in South Detroit or something
+He took the midnight tray going anywhere"""
+journey = (journey\
+    .replace("tone", "town")
+    .replace("whirl", "world")
+    .replace("tray", "train")
+    .replace("seedy", "city")
+    .replace(" or something", " ")
+)
+print(journey)
