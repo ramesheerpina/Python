42c57f611c104277a25c64b0f15c2be580e81a3a
reerpina <ramesh.eerpina@gmail.com>
2019-04-12 11:43:18 -0400

    Deleted name and travel destinations

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,8 +1,3 @@
-
-
-name = input("Please enter Your Name: ")
-print("Hello " + name + " Its nice to meet you today")
-
 #price = int(input("Please enter the total \n"))
 price = 10
 taxrate = 0.13
@@ -11,8 +6,5 @@ total = price+ subtotal
 print("your bill total is ")
 print(total)
 
-travelDestinations = ["New York", "Toronto", "San Fransisco", "Vancouver"]
-for city in travelDestinations:
-    print(city + " seems like a cool place ot travel")
 
 
