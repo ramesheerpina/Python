bb5151723b4fb17caa3a13e6d9b6144d33c42304
reerpina <ramesh.eerpina@gmail.com>
2019-04-12 11:34:14 -0400

    Added cities to travel

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,15 +1,18 @@
-price = int(input("Please enter the total \n"))
+
 
 name = "Ram"
 print("Hello " + name + " Its nice to meet you today")
 
-#price = 10
+#price = int(input("Please enter the total \n"))
+price = 10
 taxrate = 0.13
 subtotal = price * taxrate
 total = price+ subtotal
 print("your bill total is ")
 print(total)
 
-
+travelDestinations = ["New York", "Toronto", "San Fransisco", "Vancouver"]
+for city in travelDestinations:
+    print(city + " seems like a cool place ot travel")
 
 
