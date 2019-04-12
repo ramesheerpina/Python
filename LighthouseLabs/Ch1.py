17c841f3692ca5cc1faf3bf1e3e076e02a6e2aa2
Ramesh <ramesh.eerpina@gmail.com>
2019-04-12 19:06:34 -0400

    added password section

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,13 +1,18 @@
 correct_email = "ram.eerpina"
+password = "abc"
 guess_count = 0
 guess_limit = 5
 while guess_count < guess_limit:
     email = input("Please enter your email: \n")
-    if email == correct_email:
-        print("Log in is Succesful")
-        break
+    if email != correct_email:
+        print("email not valid")
     else:
-        print("Incorrect login")
-        guess_count += 1
+        pwd = input("Please enter your password: \n")
+        if password == pwd:
+            print("Login Successful")
+            break
+        else:
+            print("Incorrect login")
+            guess_count += 1
 else:
     print("Maximum Attempts Reached")
