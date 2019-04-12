95eea47aafbb2d7df6fda4342e0490f410eec797
reerpina <ramesh.eerpina@gmail.com>
2019-04-12 13:38:08 -0400

    writing multiline code using \n

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,3 +1,1 @@
-print("I said 'Can\'t we just leave this place'")
-print('I said "Can\'t we just leave this place"')
-# using \ tells python the character immediately after me shouls be interpreted differently
+print ("Hey Jude \nDon't make it bad \nAnd make it better")
