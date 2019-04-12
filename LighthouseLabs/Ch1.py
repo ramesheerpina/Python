dbe4667db4a8b92f24fc1e12783f24d445bfb0d7
reerpina <ramesh.eerpina@gmail.com>
2019-04-12 15:39:01 -0400

    math functions

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,14 +1,10 @@
-journey = """Just a small tone girl
-Leaving in a lonely whirl
-She took the midnight tray going anywhere
-Just a seedy boy
-Bored and raised in South Detroit or something
-He took the midnight tray going anywhere"""
-journey = (journey\
-    .replace("tone", "town")
-    .replace("whirl", "world")
-    .replace("tray", "train")
-    .replace("seedy", "city")
-    .replace(" or something", " ")
-)
-print(journey)
+a=13
+b=5
+#sum
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
