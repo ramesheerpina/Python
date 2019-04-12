0cd49853c774d066f3ca8b435f879d1d4ba72be9
reerpina <ramesh.eerpina@gmail.com>
2019-04-12 11:12:14 -0400

    Moved tip to bottom

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,5 +1,8 @@
 price = int(input("Please enter the total \n"))
 
+name = "Ram"
+print("Hello " + name + " Its nice to meet you today")
+
 #price = 10
 taxrate = 0.13
 subtotal = price * taxrate
@@ -7,5 +10,6 @@ total = price+ subtotal
 print("your bill total is ")
 print(total)
 
-name = "Ram"
-print("Hello " + name + " Its nice to meet you today")
+
+
+
