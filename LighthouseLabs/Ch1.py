2d9149d652a88cc82092a509eb2a9983c3e62802
Ramesh <ramesh.eerpina@gmail.com>
2019-04-12 20:48:03 -0400

    mortagage downpayment calculator

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,26 +1,10 @@
-correct_email = "ram.eerpina"
-password = "abc"
-email_count = 0
-email_limit = 3
-pwd_count = 0
-pwd_attempts = 3
-while email_count < email_limit:
-    email = input("Please enter your email: \n")
-    email_count += 1
-    if email == correct_email:
-        print("Valid Email")
-        while pwd_count < pwd_attempts:
-            pwd = input("Please enter your password: \n")
-            pwd_count += 1
-            if pwd == password:
-                print("Login Successful")
-                break
-            else:
-                print("Login Failed, Wrong Password")
-        else:
-            print("Maximum Password attempts Reached")
-        break
-    else:
-        print("Wrong email")
+houseprice = 100000
+userCreditRating = input("Please enter user credit rating (bad, fair, good, excellent: \n ")
+if userCreditRating == "bad":
+    print("Downpayment will be " + str(round(houseprice*0.2)))
+elif userCreditRating == "fair":
+    print("Downpayment will be " + str(houseprice*0.15))
+elif userCreditRating == "good":
+    print("Downpayment will be " + str(houseprice*0.1))
 else:
-    print("Maximum Email Attempts Reached")
+    print("Downpayment will be " + str(round(houseprice*0.05)))
