62f17bd22e1a2ddd33315625a164929b52d75c7f
Ramesh <ramesh.eerpina@gmail.com>
2019-04-12 23:28:38 -0400

    Word Replace Game (Excercise)

--- LighthouseLabs/Ch1.py
+++ LighthouseLabs/Ch1.py
@@ -1,8 +1,1 @@
-input1 = input("Enter name of a place: \n")
-input2 = input("Enter name of abject: \n")
-game ="Just as I arrived at place, I realized I had forgotten my object"
-sentence = (game\
-    .replace("place",input1)
-    .replace("object",input2)
-            )
-print(sentence)
+list is a collection of objects or strings or numbers
