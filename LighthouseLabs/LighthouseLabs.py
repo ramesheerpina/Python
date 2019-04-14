871a0083e7bff2221e697e45893c3adcae05bbd9
Ramesh <ramesh.eerpina@gmail.com>
2019-04-14 10:03:53 -0400

    see what happened to last print statement when it is indented. Indentation is very important in python

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -3,4 +3,4 @@ clothing = ["shirt", "pants", "socks"]
 for item in clothing:
     foldeditem = "folded " + item
     print(foldeditem)
-print("Completed folding all items")
+    print("Completed folding all items")
