6faec10e12b1586efd351d872ead8a58a9a6e5ab
Ramesh <ramesh.eerpina@gmail.com>
2019-04-14 10:11:08 -0400

    iterate over list and write secondary data in the loop

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,6 +1,14 @@
-# Loops are used to a task repeatedly
-clothing = ["shirt", "pants", "socks"]
-for item in clothing:
-    foldeditem = "folded " + item
-    print(foldeditem)
-    print("Completed folding all items")
+instructionsteps = [
+    "turn left",
+    "go straight for 2 blocks",
+    "turn right",
+    "keep going until you see the dog statue",
+    "turn right",
+    "turn right again",
+    "park right on the street"
+]
+
+instruction = "First, "
+for nextinstruction in instructionsteps:
+    instruction = instruction + nextinstruction + " then "
+print(instruction, "you are there")
