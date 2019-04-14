e1f87d0abf2ff654f75368abfdd5f2c9631f023a
Ramesh <ramesh.eerpina@gmail.com>
2019-04-14 10:02:55 -0400

    simple loop. Everything in the loop has to be indented

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,1 +1,6 @@
 # Loops are used to a task repeatedly
+clothing = ["shirt", "pants", "socks"]
+for item in clothing:
+    foldeditem = "folded " + item
+    print(foldeditem)
+print("Completed folding all items")
