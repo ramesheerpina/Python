83933a147f3bf83c8d7f3f25cf9462f3e44ee4e1
Ramesh <ramesh.eerpina@gmail.com>
2019-04-14 10:38:36 -0400

    using time function to see the increase in real time

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,4 +1,9 @@
+
+import time
 bacteria ="\U0001F952"
-for generation in range(0,10):
-    bacteria = bacteria+bacteria
-print(bacteria)
+generations = 10
+for generation in range(0,generations):
+    bacteria = bacteria*2
+    print(generation)
+    print(bacteria)
+    time.sleep(0.3)
