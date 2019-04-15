2411ed8573bf87e29ea89b9910cdfd94f51184c7
reerpina <ramesh.eerpina@gmail.com>
2019-04-15 16:22:13 -0400

    refactoring the word game

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,16 +1,15 @@
-instructionsteps = [
-    "turn left",
-    "go straight for 2 blocks",
-    "turn right",
-    "keep going until you see the dog statue",
-    "turn right",
-    "turn right again",
-    "park right on the street"
-]
+proseString = """
+Hi Dear,
 
+Just writing to let you know that I have quit my job as a OCCUPATION and I am moving to 
+COUNTRY. The truth is, I have always been passionate about PLURAL_NOUN, and COUNTRY is the best place in the world to 
+build a career around them. I will need a start small-- At first, all i will be allow to do is VERB
+near them, but when people see how ADJECTIVE I can be, I am sure to rise to the top.
+ 
+ Dont worry about me, and tell dad to take good care of my PERSONAL_ITEM. I will be sure to call every 
+ HOLIDAY
+ """
 
-for nextinstruction in instructionsteps:
-    instructionScreemed = []
-    upperinstruction = nextinstruction.upper()
-    instructionScreemed.append(upperinstruction)
-print(instructionScreemed, "you are there")
+userInput = input("Please provide a holiday")
+newProseString = proseString.replace("HOLIDAY", userInput)
+print(newProseString)
