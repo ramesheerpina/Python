622b0a12dc7d391a8bed63bf24b76fab3b4a310e
Ramesh <ramesh.eerpina@gmail.com>
2019-04-14 22:30:11 -0400

    combining two lists using a for loop

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,9 +1,24 @@
+actors = [
+    "Nathan Fillion",
+    "Gina torres",
+    "Alan Tudyk",
+    "Morena Baccrin",
+    "Adam Baldwin"
+]
+roles = [
+    "Capatain Malcolm Reynolds",
+    "Zoe Wasburn",
+    "Hoban Washburn",
+    "Inara Serra",
+    "Jayne Cobb"
+]
+print("Featuring \n"
+      "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+actornum = 0
+rolenum = 0
+for actor in actors:
+    print((actors[actornum])+ " as " + (roles[rolenum]))
+    actornum = actornum + 1
+    rolenum = rolenum + 1
 
-import time
-bacteria ="\U0001F952"
-generations = 10
-for generation in range(0,generations):
-    bacteria = bacteria*2
-    print(generation)
-    print(bacteria)
-    time.sleep(0.3)
+
