96befc667d3cb67b0401af64fd7bfc8b74036917
reerpina <ramesh.eerpina@gmail.com>
2019-04-16 12:16:41 -0400

    password checker using if else and loop

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,25 +1,13 @@
-# write a program to find 'alomond milk' and 'right logo'
-#now add else statment to know how many cartons have been looked at before getting the right one
+# excercise = password checker
 
-cartoons = [
-    ["not almond milk", 'wrong logo'],
-    ["not almond milk", 'wrong logo'],
-    ["almond milk", 'wrong logo'],
-    ["almond milk", 'right logo'],
-    ["almond milk", 'wrong logo']
-]
-cart = []
-wrongcartoons = 0
-for carton in cartoons:
-    typeofMilk = carton[0]
-    logo = carton[1]
-    if typeofMilk is 'almond milk' and logo is 'right logo':
-        cart.append(carton)
+pwd = ("123456")
+initialcount = 0
+finalcount = 5
+while initialcount < finalcount:
+    initialcount += 1
+    password = input("Please enter your password")
+    if password == pwd:
+        print("You are in")
         break
     else:
-        wrongcartoons += 1
-
-if len(cart) is 0:
-    cart.append("beer")
-
-print(" I looked at " + str(wrongcartoons) + " cartons that were not almond milk and with out the logo")
+        print("WRONG PASSWORD")
