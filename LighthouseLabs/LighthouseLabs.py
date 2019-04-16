298b964dbc8cf577ee915469603a253f66015a72
reerpina <ramesh.eerpina@gmail.com>
2019-04-16 11:53:20 -0400

    if and else

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,4 +1,5 @@
 # write a program to find 'alomond milk' and 'right logo'
+#now add else statment to know how many cartons have been looked at before getting the right one
 
 cartoons = [
     ["not almond milk", 'wrong logo'],
@@ -8,13 +9,17 @@ cartoons = [
     ["almond milk", 'wrong logo']
 ]
 cart = []
+wrongcartoons = 0
 for carton in cartoons:
     typeofMilk = carton[0]
     logo = carton[1]
     if typeofMilk is 'almond milk' and logo is 'right logo':
         cart.append(carton)
         break
+    else:
+        wrongcartoons += 1
+
 if len(cart) is 0:
     cart.append("beer")
 
-print(cart)
+print(" I looked at " + str(wrongcartoons) + " cartons that were not almond milk and with out the logo")
