7a75a156ec0f268069a61ef35c65e4d9eebb2fcc
reerpina <ramesh.eerpina@gmail.com>
2019-04-16 14:30:53 -0400

    password checker - character length and match

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -5,9 +5,23 @@ initialcount = 0
 finalcount = 5
 while initialcount < finalcount:
     initialcount += 1
-    password = input("Please enter your password")
-    if password == pwd:
-        print("You are in")
+    password1 = input("Please enter your password")
+    if len(password1) < 8:
+        print("password too short")
+
+    elif len(password1) > 8:
+        print("password too long")
+
+    elif len(password1) == 8:
+        print("you password is 8 characters")
+        while initialcount < finalcount :
+            initialcount += 1
+            password2 = input("Please enter your password again")
+            if initialcount < finalcount and password1 == password2:
+                print("You have succesfully set your password")
+                break
+            if initialcount < finalcount and password1 != password2:
+                print("passwords didnt match")
+            else:
+                print("passwords didnt match and maximum attempts reached")
         break
-    else:
-        print("WRONG PASSWORD")
