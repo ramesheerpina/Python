a8b874576452fbc724313e7ed7e5919e378db159
Ramesh <ramesh.eerpina@gmail.com>
2019-04-15 22:08:24 -0400

    logic

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,15 +1,1 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-vowels = "aeiou"
-lphbet = alphabet
-for vowel in vowels:
-    lphbet = lphbet.replace(vowel, "")
-
-print(lphbet)
-
-favourtieAlphabet = alphabet
-favouriteLetters = 'gsbrox'
-
-for letter in favouriteLetters:
-    favourtieAlphabet = favourtieAlphabet.replace(letter, letter.upper())
-
-print(favourtieAlphabet)
+# True or False are bolean values
