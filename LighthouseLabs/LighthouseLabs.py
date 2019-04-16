0c6528092dc970433b7f9c794fe8b8e770c1f824
reerpina <ramesh.eerpina@gmail.com>
2019-04-16 11:47:57 -0400

    logic and if statement

--- LighthouseLabs/LighthouseLabs.py
+++ LighthouseLabs/LighthouseLabs.py
@@ -1,4 +1,20 @@
-# True or False are bolean values
-a = 10
-b = 20
-print(a is b)
+# write a program to find 'alomond milk' and 'right logo'
+
+cartoons = [
+    ["not almond milk", 'wrong logo'],
+    ["not almond milk", 'wrong logo'],
+    ["almond milk", 'wrong logo'],
+    ["almond milk", 'right logo'],
+    ["almond milk", 'wrong logo']
+]
+cart = []
+for carton in cartoons:
+    typeofMilk = carton[0]
+    logo = carton[1]
+    if typeofMilk is 'almond milk' and logo is 'right logo':
+        cart.append(carton)
+        break
+if len(cart) is 0:
+    cart.append("beer")
+
+print(cart)
