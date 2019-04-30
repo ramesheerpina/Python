733e2b1d266af3e3417b9c460f0dcf8c95d5061f
reerpina <ramesh.eerpina@gmail.com>
2019-04-30 14:50:41 -0400

    Different ways to write classes

--- Classes.py
+++ Classes.py
@@ -6,12 +6,25 @@ x= MyClass(3.0, -4.5)
 print(x.r)
 print(x.i)
 
-class Snake:
-    def __init__(self, name):
-        self.name = name
-python = Snake("Python")
-print(python.name)
-
+#simple class
 class NewSnake:
     name = "newPython"
 print(NewSnake.name)
+
+#class with a method defined in it
+class Snake1:
+    def change_name(self, name1):
+        self.name = name1
+snake1 = Snake1()
+snake1.change_name("Anaconda")
+print(snake1.name)
+
+#__init__can be used to pass attributes at tuntime
+class Snake:
+    def __init__(self, named):
+        self.name = named
+python = Snake("Python")
+print(python.name)
+anaconda = Snake("Anaconda")
+print(anaconda.name)
+
