ecf82de5fa8ea72f5ad1da5f50396f38c42b9754
reerpina <ramesh.eerpina@gmail.com>
2019-05-01 14:57:43 -0400

    classes - inheritence

--- Classes.py
+++ Classes.py
@@ -1,30 +1,22 @@
-class MyClass:
-    def __init__(self, realpart, imagpart):
-        self.r = realpart
-        self.i = imagpart
-x= MyClass(3.0, -4.5)
-print(x.r)
-print(x.i)
+class Rocket:
+    def __init__(self, name, distance):
+        self.named = name
+        self.distanced = distance
+    def launch(self):
+        return("%s has reached %s" % (self.named, self.distanced))
 
-#simple class
-class NewSnake:
-    name = "newPython"
-print(NewSnake.name)
 
-#class with a method defined in it
-class Snake1:
-    def change_name(self, name1):
-        self.name = name1
-snake1 = Snake1()
-snake1.change_name("Anaconda")
-print(snake1.name)
 
-#__init__can be used to pass attributes at tuntime
-class Snake:
-    def __init__(self, named):
-        self.name = named
-python = Snake("Python")
-print(python.name)
-anaconda = Snake("Anaconda")
-print(anaconda.name)
+class MarsRover(Rocket): #inherited from the base class
+    def __init__(self, name, distance, maker):
+        Rocket.__init__(self, name, distance)
+        self.maker = maker
 
+    def get_maker(self):
+        return "%s Launched by %s has reached %s" %(self.named, self.maker, self.distanced)
+
+if __name__ == "__main__":
+    rockey = Rocket("Apollo", "moon")
+    print(rockey.launch())
+    x = MarsRover("marsrover", "low earth orbit", "ISRO")
+    print(x.get_maker())
