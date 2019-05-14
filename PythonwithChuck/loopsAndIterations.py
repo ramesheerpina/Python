fef1883fb29c95fe456d25bdf7553192f28f472a
ramesheerpina <ramesh.eerpina@gmail.com>
2019-05-14 14:52:03 -0400

    Delete loopsAndIterations.py

--- PythonwithChuck/loopsAndIterations.py
+++
@@ -1,8 +0,0 @@
-smallest = None
-for value in [10, 12, 5, 89, 9, 2, 67]:
-    if smallest is None:
-        smallest = value
-    elif value < smallest:
-        smallest = value
-    print(smallest, value)
-print("After", smallest)
